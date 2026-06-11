04bb284cf37a50593c515ea49d3e78935a392d37
qwhekuh-sudo <qwhekuh@gmail.com>
2026-06-11 14:27:10 +0000

    Add files via upload

---
+++ java3.java
@@ -0,0 +1,14 @@
+public class java3 {
+    static void main(String[] args) {
+        System.out.println("x                        x");
+        System.out.println("x                        x");
+        System.out.println("x      xxxxxxxxxx        x");
+        System.out.println("xxxxxx x        x    xxxxx");
+        System.out.println("x    x x        x    x   x");
+        System.out.println("x    x x        x    x   x");
+    }
+
+}
+
+
+
