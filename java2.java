197ffe15893905e33631a964e46ebe9a25d95aa5
qwhekuh-sudo <qwhekuh@gmail.com>
2026-06-11 14:25:55 +0000

    Add files via upload

---
+++ java2.java
@@ -0,0 +1,16 @@
+public class java2 {
+    public void main(String [] args){
+    System.out.println("         T");
+    System.out.println("        TTT");
+    System.out.println("       TTTTT");
+    System.out.println("      TTTTTTT");
+    System.out.println("     TTTTTTTTT");
+    System.out.println("    TTTTTTTTTTT");
+    System.out.println("   TTTTTTTTTTTTT");
+
+}
+
+}
+
+
+
